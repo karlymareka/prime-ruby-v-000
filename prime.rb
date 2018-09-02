84be95159efd55c251e4b878bada33e722c7fd5e
Karly Mareka <karly.mareka@gmail.com>
2018-09-02 00:26:46 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,6 @@
 # Add  code here!
 def prime?(integer)
-  known_primes = [2, 3, 5, 7, 11, 13, 17, 19, 23]
+  known_primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53]
   if integer == 0 || integer == 1 
     return false
   elsif integer < 0 
