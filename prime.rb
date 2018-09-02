07f3edd0aae278a77bfbc9dbf5c62bb105c024ee
Karly Mareka <karly.mareka@gmail.com>
2018-09-02 00:04:03 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,5 +1,7 @@
 # Add  code here!
 def prime(integer)
+  if integer == 0 || integer == 1 
+    false 
   if integer % 2 == 0 || integer % 3 == 0 ||
     integer % 5 == 0 || integer % 7 == 0 ||
     integer % 11 == 0 || integer % 
